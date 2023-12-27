4c53a7c3dd31a7ac4f31a636d0ccfa95ed6244d6
Colala <antt3215@gmail.com>
2023-12-27 14:44:07 +0500

    decision the first task

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+// Вводим число
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+// Составляем алгаритм по кратности чисел 7 и 23
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine($"a = {num} => да");
+}
+else
+{
+    Console.WriteLine($"a = {num} => нет");
+}
